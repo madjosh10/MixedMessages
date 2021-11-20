6c75b638a7166949c76fab2c41e178508743f74f
madjosh10 <madrigaljosh10@yahoo.com>
2021-11-20 16:20:10 -0600

    adding markdown lang

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MixedMessages
+
+### Project Objectives:
+* Build a message generator program using JavaScript
+* Use Git version control
+* Use command line
+* Develop locally on your computer
---
+++ mixed-messages.js
@@ -0,0 +1,5 @@
+const arrayOfQuotes = ['hello', 'this', 'hernia']
+let randomIndex = Math.floor(Math.random() * arrayOfQuotes.length);
+
+console.log(randomIndex);
+console.log(arrayOfQuotes[randomIndex]);
